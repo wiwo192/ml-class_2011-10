18ac0a8910d6ad872c7669a6c727da86cdc6d370
Wind Wolf <trashcan192@hotmail.com>
2011-11-28 00:04:54 -0800

    Add: hw6 solutions w/o ungraded

--- hw6/mlclass-ex6/dataset3Params.m
+++ hw6/mlclass-ex6/dataset3Params.m
@@ -9,7 +9,10 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
+
+C_vals = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_vals = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +26,19 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
-
+% E = zeros(length(C_vals), length(sigma_vals));
+% 
+% for i = 1:length(C_vals)
+%   for j = 1:length(sigma_vals)
+%     C = C_vals(i);
+%     sigma = sigma_vals(j);
+% 
+%     model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+%     pred_y = svmPredict(model, Xval);
+%     E(i, j) = mean(double(pred_y ~= yval));
+% 
+%   endfor
+% endfor
 
 % =========================================================================
 
